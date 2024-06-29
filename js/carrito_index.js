c655d1004aa609fb21938287f1ddf3c5b406a372
Erick Aguilar <erick.aguilarr@gmail.com>
2024-06-29 12:42:26 -0600

    Carrito Dividido

--- js/carrito_index.js
+++ js/carrito_index.js
@@ -1,16 +1,4 @@
-let body = document.querySelector('body');
-let iconCart = document.querySelector('.icon-cart');
-let closeCart = document.querySelector('.close');
-let listCartHTML = document.querySelector('.listCart');
-
-iconCart.addEventListener('click', () => {
-    body.classList.toggle('showCart');
-})
-closeCart.addEventListener('click', () => {
-    body.classList.toggle('showCart');
-})
-
-const addCartToHTML = () => {
+const addCartToHTML_index = () => {
     listCartHTML.innerHTML = '';
     let totalQuantity = 0;
     if(sistema.carrito.length > 0){
@@ -40,51 +28,6 @@ const addCartToHTML = () => {
         })
     }
     //iconCartSpan.innerText = totalQuantity;
-}
-
-const addCartToHTML_index = () => {
-    listCartHTML.innerHTML = '';
-    let totalQuantity = 0;
-    if(sistema.carrito.length > 0){
-        sistema.carrito.forEach(item => {
-            totalQuantity = totalQuantity +  item.cantidad;
-            let newItem = document.createElement('div');
-            newItem.classList.add('item');
-            newItem.dataset.id = item.id;
-
-            let positionProduct = sistema.productos.findIndex((value) => value.id == item.id);
-            let info = sistema.productos[positionProduct];
-            listCartHTML.appendChild(newItem);
-            newItem.innerHTML = `
-            <div class="image">
-                    <img src="./assets/images/${info.imagen}.webp">
-                </div>
-                <div class="name">
-                Iphone ${info.modelo}, ${info.color}, ${info.capacidad} GB
-                </div>
-                <div class="totalPrice">$${info.precio * item.cantidad}</div>
-                <div class="quantity">
-                    <span class="minus"><</span>
-                    <span>${item.cantidad}</span>
-                    <span class="plus">></span>
-                </div>
-            `;
-        })
-    }
-    //iconCartSpan.innerText = totalQuantity;
-}
-
-function cantidad_carrito() {
-    total_articulos = 0
-    sistema.carrito.forEach((elm) =>{
-        total_articulos = total_articulos + elm.cantidad
-    })
-    return total_articulos
-}
-
-function actualizar_icono_carrito(){
-    let total = cantidad_carrito()
-    document.querySelector(".cantidad-carrito").innerText = total
 }
 
 listCartHTML.addEventListener('click', (event) => {
@@ -118,7 +61,7 @@ const changeQuantityCart = (product_id, type) => {
                 break;
         }
     }
-    addCartToHTML();
+    addCartToHTML_index();
     actualizar_icono_carrito()
     //addCartToMemory();
     let sistema_texto = JSON.stringify(sistema) // el carrito se convierte a texto para poderlo almacenar en el local storage
